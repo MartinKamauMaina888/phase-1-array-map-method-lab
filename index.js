ff0895e986cf3f110c79a76d6bbb93d6280d2c0c
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-12-09 11:56:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,76 @@
+///////////////////////////////////////////Test examples/////////////////////////////////////////////////////////////////////////
+
+function map(array,callback){
+
+  const arrayData=[]
+  for(const el of array){
+      arrayData.push(callback(el))
+  }
+
+  return arrayData
+}
+
+
+var x= map([2,3,4],(eachnum)=>{
+
+  var answer=parseInt(eachnum) * parseInt(eachnum)
+  return answer
+})
+
+// map([1, 2, 3], function (num) {
+//   console.log(parseInt(num) * parseInt(num));
+// });
+
+// console.log(x);
+
+///////////////////////////////////////////Assignments////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+"what does the this keyword mean?",
+"What is the Constructor OO pattern?",
+"implementing Blockchain Web API",
+"The Test Driven Development Workflow",
+"What is NaN and how Can we Check for it",
+"What is the difference between stopPropagation and preventDefault?",
+"Immutable State and Pure Functions",
+"what is the difference between == and ===?",
+"what is the difference between event capturing and bubbling?",
+"what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+
+const newCapsTutorial = tutorials.map((tutorial)=>{
+
+  const newtutorial = tutorial.split(" ");
+for (var i = 0; i < newtutorial.length; i++) {
+  newtutorial[i] = newtutorial[i].charAt(0).toUpperCase() + newtutorial[i].slice(1);
+
 }
+
+const answer = newtutorial.join(" ");
+
+console.log(answer);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
